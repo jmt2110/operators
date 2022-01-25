937de242a6b88b35deffa37b3ced317c04b2dd14
jmt2110 <jthomas863@students.mchenry.edu>
2022-01-24 19:35:16 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var a = 13
+    var b = 4
+
+    println("13 + 4 = " + (a + b))
+    println("13 - 4 = " + (a - b))
+    println("13 * 4 = " + (a * b))
+    println("13 / 4 = " + (a / b))
+    println("13 % 4 = " + (a % b))
+}
